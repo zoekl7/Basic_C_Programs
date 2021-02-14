7b2a2fe4d4a2c760b3376a490eb4a1fa96ddd089
Zoe <zoeoklein@gmail.com>
2021-02-13 17:40:37 -0800

    C# practice and assignments

---
+++ AcmeAccounting/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AcmeAccounting
+{
+    class Program
+    {
+        static void Main()
+        {
+            string txt = "Welcome to Acme Accounting Systems \n Remember, we're \"accounting\" on you!";
+            Console.WriteLine(txt);
+            Console.ReadLine();
+        }
+    }
+}
