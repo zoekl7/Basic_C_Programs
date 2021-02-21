a1fbc7f5e45587945a3312a512d38af694092a8d
Zoe <zoeoklein@gmail.com>
2021-02-20 19:07:02 -0800

    Stringbuilder assignment

---
+++ StringbuilderAssignment/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringbuilderAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // concatenate strings and use "ToUpper" method
+            Console.WriteLine("What is your name?");
+            string name = Console.ReadLine();
+            name = name.ToUpper();
+            Console.WriteLine("Favorite fictional place?");
+            string place = Console.ReadLine();
+            Console.WriteLine("Enter an adjective to describe this place:");
+            string adjective = Console.ReadLine();
+            string greeting = "Hello, " + name + " of " + adjective + " " + place + "!";
+            Console.WriteLine(greeting);
+            Console.WriteLine("(press enter to continue)");
+            Console.ReadLine();
+
+            // StringBuilder
+            StringBuilder vforV = new StringBuilder();
+            vforV.Append("Remember, remember, the Fifth of November, ");
+            vforV.Append("the Gunpowder Treason and Plot. I know of no reason why the Gunpowder Treason ");
+            vforV.Append("Should ever be forgot...");
+            Console.WriteLine(vforV);
+            Console.Read();
+        }
+    }
+}
---
+++ Strings/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Strings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "Skywalker";
+            string quote = "The man said, \"Hello, Skywalker.\"\nHello on a new line.\n\tHello on a tab.";
+            // string fileName = "C:\\Users\\Zoe";
+            string fileName = @"C:\Users\Zoe";
+
+            // bool trueOrFalse = name.Contains("s");
+            bool trueOrFalse = name.EndsWith("s");
+
+            int length = name.Length;
+
+            name = name.ToUpper();
+
+            Console.WriteLine(quote);
+            Console.WriteLine(fileName);
+            Console.WriteLine(trueOrFalse);
+            Console.WriteLine(length);
+            Console.WriteLine(name);
+
+            // create instance of class StringBuilder:
+            StringBuilder sb = new StringBuilder();
+            sb.Append("My name is Zoe");
+
+            Console.WriteLine(sb);
+            Console.ReadLine();
+        }
+    }
+}
