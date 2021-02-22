0c05e2307292fec474e1a57a4628215ea17da091
Zoe <zoeoklein@gmail.com>
2021-02-22 14:53:36 -0800

    Exception Handling

---
+++ ExceptionHandling/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        try
+        {
+            Console.WriteLine("Pick a number.");
+            int numberOne = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Pick another number.");
+            int numberTwo = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Dividing the two...");
+            int numberThree = numberOne / numberTwo;
+            Console.WriteLine(numberOne + " divded by " + numberTwo + " equals " + numberThree);
+        }
+        catch (FormatException ex)
+        {
+            Console.WriteLine("Please type a whole number.");
+            return;
+        }
+        catch (DivideByZeroException ex)
+        {
+            Console.WriteLine("Please don't divide by zero.");
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+        finally
+        {
+            Console.ReadLine();
+        }
+            
+    }
+}
+
---
+++ ExceptionHandlingAssign/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExceptionHandlingAssign
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> dividends = new List<int> { 12, 20, 23, 145, 238, 429, 821, 1208, 5252, 15243, 82411 };
+
+            try
+            {
+                Console.WriteLine("Please enter a number to divide by:");
+                int divisor = Convert.ToInt32(Console.ReadLine());
+                foreach (int dividend in dividends)
+                {
+                    int quotient = dividend / divisor;
+                    int remainder = dividend % divisor;
+                    Console.WriteLine(dividend + " divided by " + divisor + " equals " + quotient + " with a remainder of " + remainder);
+                }
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Please enter a whole number!");
+            }
+            catch (DivideByZeroException)
+            {
+                Console.WriteLine("Please don't divide by zero!");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                Console.WriteLine("\"Finally\" out of try-catch block! Program execution finished");
+                Console.ReadLine();
+            }
+        }
+    }
+}
