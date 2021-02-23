065f2a5c5d90feb37c6e1e456658b51f28f80eb8
Zoe <zoeoklein@gmail.com>
2021-02-23 12:50:29 -0800

    void method

---
+++ MethodAndClassesVoid/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodAndClassesVoid
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            mathOperation.mathOp(3, 4);
+            mathOperation.mathOp(numOne: 5, numTwo: 32);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ MethodAndClassesVoid/mathOperation.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodAndClassesVoid
+{
+    public class mathOperation
+    {
+        public static void mathOp(int numOne, int numTwo)
+        {
+            Console.WriteLine(numOne * 2 + 13);
+            Console.WriteLine(numTwo);
+        }
+    }
+}
