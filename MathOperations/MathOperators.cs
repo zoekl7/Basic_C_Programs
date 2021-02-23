97ed5108e87333b3fb2c03e83cdf1eeaafed73b2
Zoe <zoeoklein@gmail.com>
2021-02-22 17:56:40 -0800

    Math

---
+++ MathOperations/MathOperators.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathOperations
+{
+    public class MathOperators
+    {
+        public static int Multiplication(int number)
+        {
+            return number * 5;
+        }
+
+        public static int Subtraction(int number)
+        {
+            return number - 7;
+        }
+
+        public static int Addition(int number)
+        {
+            return number + 700;
+        }
+    }
+}
---
+++ MathOperations/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathOperations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter an integer:");
+            int number = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine(number + " multiplied by 5 equals " + MathOperators.Multiplication(number));
+            Console.WriteLine(number + " minus 7 equals " + MathOperators.Subtraction(number));
+            Console.WriteLine(number + " plus 700 equals " + MathOperators.Addition(number));
+
+            Console.ReadLine();
+        }
+    }
+}
