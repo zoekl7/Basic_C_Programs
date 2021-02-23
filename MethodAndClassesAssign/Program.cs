8e37e1596194593d92c7faebb047323c8be38bcb
Zoe <zoeoklein@gmail.com>
2021-02-22 18:30:05 -0800

    more math operations

---
+++ MethodAndClassesAssign/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodAndClassesAssign
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberOne = MathOperations.mathOp(70);
+            Console.WriteLine(numberOne);
+
+            double numberTwo = MathOperations.mathOp(13.77);
+            Console.WriteLine(numberTwo);
+
+            int numberThree = MathOperations.mathOp("7");
+            Console.WriteLine(numberThree);
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ MethodAndClassesAssign/mathOperation.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodAndClassesAssign
+{
+    public class MathOperations
+    {
+        private static int num;
+
+        public static int mathOp(int number)
+        {
+            return number * 2;
+        }
+
+        public static double mathOp(double number)
+        {
+            return number / 3;
+        }
+
+        public static int mathOp(string number)
+        {
+            num = Convert.ToInt32(number);
+            return num + 666;
+        }
+
+    }
+}
