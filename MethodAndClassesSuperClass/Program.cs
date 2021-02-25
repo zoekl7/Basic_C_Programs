275388c122abf25d4634c97b6c8b154c90d66748
Zoe <zoeoklein@gmail.com>
2021-02-24 17:02:18 -0800

    superclass assignment

---
+++ MethodAndClassesSuperClass/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodAndClassesSuperClass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee test = new Employee() { FirstName = "Sample", LastName = "Student" };
+            test.SayName();
+            Console.WriteLine(test.Id);
+            Console.ReadLine();
+        }
+    }
+}
--- TwentyOne/Program.cs
+++ TwentyOne/Program.cs
@@ -10,17 +10,25 @@ namespace TwentyOne
     {
         static void Main(string[] args)
         {
-            Deck deck = new Deck();
+            // initialize the object
+            //Card card = new Card() { Face = "King", Suit = "Spades" };
+            TwentyOneGame game = new TwentyOneGame();
+            game.Players = new List<string>() { "Zoe", "Luna", "Emma" };
+            game.ListPlayers();
+            game.Play();
+            Console.ReadLine();
+
+            //Deck deck = new Deck();
             //deck = Shuffle(deck);
             //deck = Shuffle(deck: deck, times: 3);
-            deck.Shuffle(3);
+            //deck.Shuffle(3);
             
-            foreach (Card card in deck.Cards)
-            {
-                Console.WriteLine(card.Face + " of " + card.Suit);
-            }
-            Console.WriteLine(deck.Cards.Count);
-            Console.ReadLine();
+            //foreach (Card card in deck.Cards)
+            //{
+            //    Console.WriteLine(card.Face + " of " + card.Suit);
+            //}
+            //Console.WriteLine(deck.Cards.Count);
+            //Console.ReadLine();
         }
 
         
