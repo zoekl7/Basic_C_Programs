4b1bc0ba62cb340a0af7a740fe99784c37dd0b5f
Zoe <zoeoklein@gmail.com>
2022-02-16 18:06:57 -0800

    working on

--- TwentyOne/Program.cs
+++ TwentyOne/Program.cs
@@ -12,23 +12,23 @@ namespace TwentyOne
         {
             // initialize the object
             //Card card = new Card() { Face = "King", Suit = "Spades" };
-            TwentyOneGame game = new TwentyOneGame();
-            game.Players = new List<string>() { "Zoe", "Luna", "Emma" };
-            game.ListPlayers();
-            game.Play();
-            Console.ReadLine();
+            //TwentyOneGame game = new TwentyOneGame();
+            //game.Players = new List<string>() { "Zoe", "Luna", "Emma" };
+            //game.ListPlayers();
+            //game.Play();
+            //Console.ReadLine();
 
-            //Deck deck = new Deck();
+            Deck deck = new Deck();
             //deck = Shuffle(deck);
             //deck = Shuffle(deck: deck, times: 3);
-            //deck.Shuffle(3);
+            deck.Shuffle(3);
             
-            //foreach (Card card in deck.Cards)
-            //{
-            //    Console.WriteLine(card.Face + " of " + card.Suit);
-            //}
-            //Console.WriteLine(deck.Cards.Count);
-            //Console.ReadLine();
+            foreach (Card card in deck.Cards)
+            {
+                Console.WriteLine(card.Face + " of " + card.Suit);
+            }
+            Console.WriteLine(deck.Cards.Count);
+            Console.ReadLine();
         }
 
         
